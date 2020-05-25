71933de6f0c7f88f640b1140ab5182bb898275d5
great_bn <great_bn@yahoo.com>
2020-05-26 00:54:21 +0700

    add listener and pool command

--- cmd/loadbalancer.go
+++ cmd/loadbalancer.go
@@ -19,6 +19,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/bizflycloud/bizflyctl/formatter"
@@ -28,6 +29,9 @@ import (
 
 var (
 	lbListHeader = []string{"ID", "Name", "Network Type", "IP Address", "Operating Status", "Type"}
+	poolListHeader = []string{"ID", "Name", "Algorithm", "Protocol", "Operating Status"}
+	listenerListHeader = []string{"ID", "Name", "Protocol", "Protocol Port", "Operating Status", "Default Pool ID"}
+
 )
 
 // serverCmd represents the server command
@@ -36,15 +40,29 @@ var lbCmd = &cobra.Command{
 	Short: "BizFly Cloud Load Balancer Interaction",
 	Long:  `BizFly Cloud Load Balancer Action: Create, List, Delete`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("server called")
+		fmt.Println("loadbalancer called")
 	},
+}
+
+var lbListenerCmd = &cobra.Command{
+	Use: "listener",
+	Short: "BizFly Cloud Load Balancer Listener Interaction",
+	Long: "BizFly Cloud Load Balancer Listener Action: Create, List, Delete, Get",
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+var lbPoolCmd = &cobra.Command{
+	Use: "pool",
+	Short: "BizFly Cloud Load Balancer Pool Interaction",
+	Long: "BizFly Cloud Load Balancer Pool Action: Create, List, Delete, Get",
+	Run: func(cmd *cobra.Command, args []string) {},
 }
 
 // deleteCmd represents the delete command
 var lbDeleteCmd = &cobra.Command{
 	Use:   "delete",
-	Short: "Delete Server",
-	Long: `Delete Server with Load Balancer ID as input
+	Short: "Delete load balancer",
+	Long: `Delete Load Balancer with Load Balancer ID as input
 Example: bizfly loadbalancer delete fd554aac-9ab1-11ea-b09d-bbaf82f02f58
 
 You can delete multiple loadbalancers with list of loadbalancer id
@@ -113,9 +131,167 @@ Example: bizfly loadbalancer get fd554aac-9ab1-11ea-b09d-bbaf82f02f58
 	},
 }
 
+
+// lbPoolDeleteCmd represents the delete command
+var lbPoolDeleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete pool in load balancer",
+	Long: `Delete Pool in a Load balancer with Pool ID as input
+Example: bizfly loadbalancer pool delete fd554aac-9ab1-11ea-b09d-bbaf82f02f58
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, ctx := getApiClient(cmd)
+		// TODO: check length of args
+		poolID := args[0]
+		fmt.Printf("Deleting pool %s \n", poolID)
+		err := client.Pool.Delete(ctx, poolID)
+		if err != nil {
+			if errors.Is(err, gobizfly.ErrNotFound) {
+				fmt.Printf("Pool %s is not found", poolID)
+				return
+			}
+		}
+	},
+}
+
+// lbPoolListCmd represents the list command
+var lbPoolListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all pools in a load balancer",
+	Long:  `List all pools in a load balancer
+Example: bizfly loadbalancer pool list <loadbalancer_id>
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, ctx := getApiClient(cmd)
+		// TODO Check length args
+		lbID := args[0]
+		pools, err := client.Pool.List(ctx, lbID, &gobizfly.ListOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		var data [][]string
+		for _, pool := range pools {
+			s := []string{pool.ID, pool.Name, pool.LBAlgorithm, pool.Protocol, pool.OperatingStatus}
+			data = append(data, s)
+		}
+		formatter.Output(poolListHeader, data)
+	},
+}
+
+// lbPoolGetCmd represents the get command
+var lbPoolGetCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get detail a pool in a load balancer",
+	Long: `Get detail a pool in a load balancer with pool ID as input
+Example: bizfly loadbalancer pool get fd554aac-9ab1-11ea-b09d-bbaf82f02f58
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 1 {
+			fmt.Printf("Unknow variable %s", strings.Join(args[1:], ""))
+		}
+		client, ctx := getApiClient(cmd)
+
+		pool, err := client.Pool.Get(ctx, args[0])
+		if err != nil {
+			if errors.Is(err, gobizfly.ErrNotFound) {
+				fmt.Printf("Pool %s not found.", args[0])
+				return
+			}
+			log.Fatal(err)
+		}
+		var data [][]string
+		data = append(data, []string{pool.ID, pool.Name, pool.LBAlgorithm, pool.Protocol, pool.OperatingStatus})
+		formatter.Output(poolListHeader, data)
+	},
+}
+
+
+// lbListenerDeleteCmd represents the delete command
+var lbListenerDeleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete listener in load balancer",
+	Long: `Delete Listener in a Load balancer with Listener ID as input
+Example: bizfly loadbalancer listener delete fd554aac-9ab1-11ea-b09d-bbaf82f02f58
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, ctx := getApiClient(cmd)
+		// TODO: check length of args
+		listenerID := args[0]
+		fmt.Printf("Deleting listener %s \n", listenerID)
+		err := client.Listener.Delete(ctx, listenerID)
+		if err != nil {
+			if errors.Is(err, gobizfly.ErrNotFound) {
+				fmt.Printf("Listener %s is not found", listenerID)
+				return
+			}
+		}
+	},
+}
+
+// lbListenerListCmd represents the list command
+var lbListenerListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all listeners in a load balancer",
+	Long:  `List all listeners in a loadbalancer
+Example: bizfly loadbalancer listener list <loadbalancer_id>
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, ctx := getApiClient(cmd)
+		// TODO Check length args
+		lbID := args[0]
+		listeners, err := client.Listener.List(ctx, lbID, &gobizfly.ListOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		var data [][]string
+		for _, listener := range listeners {
+			s := []string{listener.ID, listener.Name, listener.Protocol, strconv.Itoa(listener.ProtocolPort), listener.OperatingStatus, listener.DefaultPoolID}
+			data = append(data, s)
+		}
+		formatter.Output(listenerListHeader, data)
+	},
+}
+
+// lbListenerGetCmd represents the get command
+var lbListenerGetCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get a listener in load balancer",
+	Long: `Get detail a listener with listener  ID as input
+Example: bizfly loadbalancer listener get fd554aac-9ab1-11ea-b09d-bbaf82f02f58
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 1 {
+			fmt.Printf("Unknow variable %s", strings.Join(args[1:], ""))
+		}
+		client, ctx := getApiClient(cmd)
+
+		listener, err := client.Listener.Get(ctx, args[0])
+		if err != nil {
+			if errors.Is(err, gobizfly.ErrNotFound) {
+				fmt.Printf("Listener %s not found.", args[0])
+				return
+			}
+			log.Fatal(err)
+		}
+		var data [][]string
+		data = append(data, []string{listener.ID, listener.Name, listener.Protocol, strconv.Itoa(listener.ProtocolPort), listener.OperatingStatus, listener.DefaultPoolID})
+		formatter.Output(listenerListHeader, data)
+	},
+}
 func init() {
 	rootCmd.AddCommand(lbCmd)
 	lbCmd.AddCommand(lbListCmd)
 	lbCmd.AddCommand(lbGetCmd)
 	lbCmd.AddCommand(lbDeleteCmd)
+
+	lbCmd.AddCommand(lbPoolCmd)
+	lbCmd.AddCommand(lbListenerCmd)
+
+	lbPoolCmd.AddCommand(lbPoolGetCmd)
+	lbPoolCmd.AddCommand(lbPoolListCmd)
+	lbPoolCmd.AddCommand(lbPoolDeleteCmd)
+
+	lbListenerCmd.AddCommand(lbListenerGetCmd)
+	lbListenerCmd.AddCommand(lbListenerDeleteCmd)
+	lbListenerCmd.AddCommand(lbListenerListCmd)
 }
