a4167fd0eaa5d9765474deaaf80de7ac4c7dc382
greatbn <great_bn@yahoo.com>
2020-06-27 01:30:41 +0700

    add flavor command

---
+++ cmd/flavor.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/bizflycloud/bizflyctl/formatter"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var flavorListHeader = []string{"ID", "Name"}
+
+// flavorCmd represents the flavor command
+var flavorCmd = &cobra.Command{
+	Use:   "flavor",
+	Short: "BizFly Cloud Flavor Interaction",
+	Long:  `BizFly Cloud Flavor Action: List Flavors`,
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+// flavorListcmd represents list all flavors
+var flavorListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all flavor of BizFly Cloud",
+	Long: `
+List all flavor of BizFly Cloud.
+Use: bizflyctl flavor list
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, ctx := getApiClient(cmd)
+		flavors, err := client.Server.ListFlavors(ctx)
+		if err != nil {
+			fmt.Printf("List flavors error %v", err)
+			os.Exit(1)
+		}
+		var data [][]string
+		for _, flavor := range flavors {
+			s := []string{flavor.ID, flavor.Name}
+			data = append(data, s)
+		}
+		formatter.Output(flavorListHeader, data)
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(flavorCmd)
+	flavorCmd.AddCommand(flavorListCmd)
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/bizflycloud/bizflyctl
 go 1.14
 
 require (
-	github.com/bizflycloud/gobizfly v0.0.0-20200514103014-5907e6882c84
+	github.com/bizflycloud/gobizfly v0.0.0-20200626173114-1b029c29ea32
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/olekukonko/tablewriter v0.0.4
 	github.com/spf13/cobra v1.0.0
