a64212f749063fd6ea6069d24dff002b94249692
greatbn <great_bn@yahoo.com>
2020-06-27 01:44:22 +0700

    add list image

---
+++ cmd/image.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/bizflycloud/bizflyctl/formatter"
+	"github.com/spf13/cobra"
+)
+
+var imageListHeader = []string{"ID", "Distribution", "Version"}
+
+// imageCmd represents the image command
+var imageCmd = &cobra.Command{
+	Use:   "image",
+	Short: "BizFly Cloud Image Interaction",
+	Long:  `BizFly Cloud Image Action: List OS Image, Create a custom image`,
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+// imageListcmd represents list all os images
+var imageListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list all os images in BizFly Cloud",
+	Long: `
+List all os images in BizFly Cloud
+Use: bizflyctl image list
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, ctx := getApiClient(cmd)
+		images, err := client.Server.ListOSImages(ctx)
+		if err != nil {
+			fmt.Printf("List os image error: %v", err)
+		}
+		var data [][]string
+		for _, image := range images {
+			osDist := image.OSDistribution
+			for _, osVer := range image.Version {
+				s := []string{osVer.ID, osDist, osVer.Name}
+				data = append(data, s)
+			}
+		}
+		formatter.Output(imageListHeader, data)
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(imageCmd)
+	imageCmd.AddCommand(imageListCmd)
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/bizflycloud/bizflyctl
 go 1.14
 
 require (
-	github.com/bizflycloud/gobizfly v0.0.0-20200626173114-1b029c29ea32
+	github.com/bizflycloud/gobizfly v0.0.0-20200626183932-6e5ef70f3977
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/olekukonko/tablewriter v0.0.4
 	github.com/spf13/cobra v1.0.0
