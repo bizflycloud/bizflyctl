f6b4d4212bb4e4f23085920382223d61218f6aa7
tung491 <dosontung007@gmail.com>
2021-05-27 20:53:04 +0700

    remove redudant print abs path

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,6 @@ import (
 	"os"
 	"strings"
 	"time"
-	"path/filepath"
 
 	"github.com/bizflycloud/gobizfly"
 	homedir "github.com/mitchellh/go-homedir"
@@ -82,12 +81,12 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	home, err := homedir.Dir()
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
 		// Find home directory.
+		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -102,9 +101,7 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		configAbsStrPath := home + viper.ConfigFileUsed()
-		configAbsPath, _ := filepath.Abs(configAbsStrPath)
-		fmt.Println("Using config file: " + configAbsPath)
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
 
