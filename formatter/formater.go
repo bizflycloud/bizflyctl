da0f7775173c1e2d4bac6e5676de583a0d2cae90
Tùng Đỗ Sơn <svtt.tungds@vccloud.vn>
2021-12-06 21:46:26 +0700

    feat: remove border in formater

--- formatter/formater.go
+++ formatter/formater.go
@@ -11,8 +11,17 @@ import (
 func Output(header []string, data [][]string) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader(header)
+	table.SetAutoWrapText(false)
+	table.SetAutoFormatHeaders(true)
+	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
 	table.SetAlignment(tablewriter.ALIGN_LEFT)
-	table.SetBorder(true)
+	table.SetCenterSeparator("")
+	table.SetColumnSeparator("")
+	table.SetRowSeparator("")
+	table.SetHeaderLine(false)
+	table.SetBorder(false)
+	table.SetTablePadding("\t")
+	table.SetNoWhiteSpace(true)
 	table.AppendBulk(data)
 	table.Render()
 }
